341640f836dd5858e44a8fff50bb0de31e613628
Sai Sashreek D <saisashreekd@gmail.com>
2025-06-12 15:54:01 +0530

    Updated UI

--- app/page.js
+++ app/page.js
@@ -1,8 +1,110 @@
+"use client";
+
 import Image from "next/image";
-import styles from "./page.module.css";
+import { useRouter } from "next/navigation";
+import { GiBookshelf } from "react-icons/gi";
 
 export default function Home() {
+  const router = useRouter();
+
+  const getStartedHandler = (e) => {
+    e.preventDefault();
+    router.push("/books");
+  };
+
   return (
-   <h1>Hello</h1>
+    <>
+      <header className="flex  flex-wrap sm:justify-start  sm:flex-nowrap z-50 w-full bg-white border-b border-gray-200 text-sm py-3 sm:py-0 dark:bg-neutral-800 dark:border-neutral-700">
+        <nav
+          className="relative  p-4 max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8"
+          aria-label="Global"
+        >
+          <div className="flex items-center justify-between">
+            <div>
+              <Image src={"/logo.svg"} alt="logo" width={150} height={150} />
+            </div>
+          </div>
+          <div className="hidden overflow-hidden transition-all duration-300 basis-full grow sm:block">
+            <div className="flex flex-col sm:flex-row sm:items-center sm:justify-end sm:ps-7 cursor-pointer">
+              <div
+                onClick={getStartedHandler}
+                className="flex items-center gap-x-2 font-medium text-gray-500 hover:text-blue-600 sm:border-s sm:border-gray-300 py-2 sm:py-0 sm:ms-4 sm:my-6 sm:ps-6 dark:border-neutral-700 dark:text-neutral-400 dark:hover:text-blue-500"
+              >
+                <GiBookshelf size={24} />
+                Get Started
+              </div>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <div className="relative overflow-hidden before:absolute before:top-0 before:start-1/2 before:bg-[url('https://preline.co/assets/svg/examples/polygon-bg-element.svg')] dark:before:bg-[url('https://preline.co/assets/svg/examples-dark/polygon-bg-element.svg')] before:bg-no-repeat before:bg-top before:bg-cover before:size-full before:-z-[1] before:transform before:-translate-x-1/2">
+        <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-10">
+          <div className="flex justify-center">
+            <h2
+              className="inline-flex items-center gap-x-2 bg-white border border-gray-200 text-sm text-gray-800 p-1 ps-3 rounded-full transition hover:border-gray-300 dark:bg-neutral-800 dark:border-neutral-700 dark:hover:border-neutral-600 dark:text-neutral-200 cursor-pointer"
+              onClick={getStartedHandler}
+            >
+              Welcome To Book Reading App - Create Now
+              <span className="py-1.5 px-2.5 inline-flex justify-center items-center gap-x-2 rounded-full bg-gray-200 font-semibold text-sm text-gray-600 dark:bg-neutral-700 dark:text-neutral-400">
+                <svg
+                  className="flex-shrink-0 size-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="m9 18 6-6-6-6" />
+                </svg>
+              </span>
+            </h2>
+          </div>
+
+          <div className="mt-5 max-w-2xl text-center mx-auto">
+            <h1 className="block font-bold text-gray-800 text-4xl md:text-5xl lg:text-6xl dark:text-neutral-200">
+              Create Something
+              <span className="bg-clip-text bg-gradient-to-tl from-blue-600 to-violet-600 text-transparent">
+                {" "}
+                With Technology
+              </span>
+            </h1>
+          </div>
+
+          <div className="mt-5 max-w-3xl text-center mx-auto">
+            <p className="text-lg text-gray-600 dark:text-neutral-400">
+              Revolutionize your content creation with our AI-powered app,
+              delivering engaging and high-quality apps in seconds.
+            </p>
+          </div>
+
+          <div className="mt-8 gap-3 flex justify-center">
+            <h2
+              className="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 hover:from-violet-600 hover:to-blue-600 border border-transparent cursor-pointer text-white text-sm font-medium rounded-md focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-4 dark:focus:ring-offset-gray-800"
+              onClick={getStartedHandler}
+            >
+              Get started
+              <svg
+                className="flex-shrink-0 size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="m9 18 6-6-6-6" />
+              </svg>
+            </h2>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
